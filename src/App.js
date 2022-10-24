ec79600d28b1fda7aed0924a26ccd4f5baa4455c
Emmanuel Nwaji <envarmp@gmail.com>
2022-10-24 10:47:53 +0100

    Added calculator functionality
    Added calculator functionality to App.js
    imported useState from react
    Used a single function with eval() to carry out arithmetic functions for addition, subtraction, division, multiplication.
    Also added functionality to clear (C), decimal point (.) and the equal-to (=).
    
    Extras:
    reduced font-sizes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,74 @@
+import { useState } from 'react';
+
+
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const opSigns = ['+', '-', '*', '/', '.', '='];
+
+  const updateCalc = value => {
+    if (
+      (opSigns.includes(value) && calc === '') || (opSigns.includes(value) && opSigns.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+
+    if (!opSigns.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  }
 
   const createDigits = () => {
     const digits = [];
 
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button key={i}>{i}</button>
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
       )
     }
     return digits;
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString()); 
+  }
+
+  const clearLast = () => {
+    if (calc == '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
   }
 
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span> 0
+          { result ? <span>({result})</span> : '' }&nbsp;
+          { calc || "0" }
         </div>
         <div className="operators">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>/</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={() => updateCalc("*")}>x</button>
+          <button onClick={() => updateCalc("/")}>/</button>
 
-          <button>DEL</button>
+          <button onClick={clearLast}>C</button>
         </div>
         <div className="digits">
           { createDigits() }
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+
+          <button onClick={calculate}>=</button>
         </div>
       </div>
     </div>
